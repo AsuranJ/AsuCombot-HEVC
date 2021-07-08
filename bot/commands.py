438ebf75982acb25d84540e2eb2631f30bf2181b
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-07-08 16:06:15 +0530

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -7,29 +7,29 @@ from bot.get_cfg import get_config
 class Command:
     START = get_config(
         "COMMAND_START",
-        "start"
+        "start1"
     )
     COMPRESS = get_config(
         "COMMAND_COMPRESS",
-        "compress"
+        "compress1"
     )
     CANCEL = get_config(
         "COMMAND_CANCEL",
-        "cancel"
+        "cancel1"
     )
     STATUS = get_config(
         "COMMAND_STATUS",
-        "status"
+        "status1"
     )
     EXEC = get_config(
         "COMMAND_EXEC",
-        "exec"
+        "exec1"
     )
     HELP = get_config(
         "COMMAND_HELP",
-        "help"
+        "help1"
     )
     UPLOAD_LOG_FILE = get_config(
         "COMMAND_UPLOAD_LOG_FILE",
-        "log"
+        "log1"
     )
